d71584777077574570ae71a6631c28a76a7240d7
CvCvCvv <decomen123@gmail.com>
2023-12-08 09:27:56 +0300

    добавлены Bybit rest client тесты

--- RBTB_ServiceStrategy/Markets/Bybit/BybitRestClient.cs
+++ RBTB_ServiceStrategy/Markets/Bybit/BybitRestClient.cs
@@ -1,17 +1,14 @@
 ﻿using BybitMapper.UTA.RestV5.Requests.Market;
 using BybitMapper.Requests;
 using RestSharp;
-using System.Drawing;
-using System.Net;
 using BybitMapper.UTA.RestV5.Data.Enums;
-using BybitMapper.UTA.RestV5.Requests.Position;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using ServerTimeResponse = BybitMapper.UTA.RestV5.Responses.Market.ServerTimeResponse;
-using BybitMapper.UTA.RestV5.Responses.Position;
-using BybitMapper.UTA.RestV5.Requests.Market.Option;
-using BybitMapper.UTA.RestV5.Responses.Market.Option;
 using BybitMapper.UTA.RestV5.Responses.Market;
+using BybitMapper.UTA.RestV5.Data.ObjectDTO.Market.Kline;
+using BybitMapper.UTA.RestV5.Data.ObjectDTO.Market.Orderbook;
+using System.Text;
 
 namespace RBTB_ServiceStrategy.Markets.Bybit
 {
@@ -19,108 +16,55 @@ namespace RBTB_ServiceStrategy.Markets.Bybit
     {
 
         private RequestArranger _arranger;
-        private CSCommon.Http.CommonHttpClient _client;
-        private string _api = "6CjIuMxhY8DAhWu03C";
-        private string _secret = "DoGlywXMEp8pvt9s5L2vYwIuoXBUnnSulHby";
+        private RestClient _client;
+
 
         private static JsonSerializerOptions jsonSerializerOptions = new()
         {
-            NumberHandling = JsonNumberHandling.AllowReadingFromString
+            NumberHandling = JsonNumberHandling.AllowReadingFromString,
+            IncludeFields = true 
         };
-        public BybitRestClient(RequestArranger ra)
-        {
-            _arranger = ra;
-        }
-        public BybitRestClient(RequestArranger ra, string api, string secret)
-        {
-            _arranger = ra;
-            _api = api;
-            _secret = secret;
-        }
 
-        public void Setup()
+        public BybitRestClient(string url, string api, string secret)
         {
-            _arranger = new(_api, _secret);
+            _client = new RestClient(url);
+            _arranger = new RequestArranger(api, secret);
         }
 
         #region [Helpers]
         private T GetContent<T>(RequestPayload request)
         {
             var arrange = _arranger.Arrange(request);
-            var content = _client.GetContent<T>(GetHttpMethod(arrange.Method), arrange.Query, null, arrange.Headers);
-            return content;
-        }
+            var restRequest = new RestRequest(arrange.Query, GetHttpMethod(arrange.Method));
+            var response = _client.Get<T>(restRequest);
 
-        private static async Task<T?> DeserializeAsync<T>(ArraySegment<byte> message, int lenght)
-        {
-            using (MemoryStream stream = new MemoryStream(message.Array, 0, lenght))
-            {
-                return await JsonSerializer.DeserializeAsync<T>(stream, jsonSerializerOptions);
-            }
+            return response.Data;
         }
-        private static T? Deserialize<T>(byte[] message)
+        private static T? Deserialize<T>(string message)
         {
-            using (MemoryStream stream = new MemoryStream(message, 0, message.Length))
-            {
-                return JsonSerializer.Deserialize<T>(stream, jsonSerializerOptions);
-            }
+
+            return JsonSerializer.Deserialize<T>(message, jsonSerializerOptions);
+
         }
-        private HttpMethod GetHttpMethod(RequestMethod method)
+        private Method GetHttpMethod(RequestMethod method)
         {
             switch (method)
             {
-                case RequestMethod.GET: return HttpMethod.Get;
-                case RequestMethod.POST: return HttpMethod.Post;
-                case RequestMethod.PUT: return HttpMethod.Put;
-                case RequestMethod.DELETE: return HttpMethod.Delete;
+                case RequestMethod.GET: return Method.GET;
+                case RequestMethod.POST: return Method.POST;
+                case RequestMethod.PUT: return Method.PUT;
+                case RequestMethod.DELETE: return Method.DELETE;
                 default: throw new NotImplementedException();
             }
         }
 
-        private long GetTime()
-        {
-            var request = new ServerTimeRequest();
-            var response = GetContent<ServerTimeResponse>(request);
-            return response.Time.Value;
-        }
         #endregion
 
 
         #region [Base]
 
-        RestClient m_RestClient;
-
-        internal void SetUrl(string restUrl)
-        {
-            m_RestClient = new RestClient(restUrl);
-        }
-
-        internal void SetProxy(string ip, string port, string username, string password)
-        {
-            try
-            {
-                var proxy = new WebProxy($"{ip}:{port}");
-                if (!string.IsNullOrWhiteSpace(username) || !string.IsNullOrWhiteSpace(password))
-                {
-                    proxy.Credentials = new NetworkCredential(username, password);
-                }
-
-                m_RestClient.Proxy = proxy;
-
-            }
-            catch (Exception ex)
-            {
-                OnLogEx(ex);
-            }
-        }
-
-        internal void ResetProxy()
-        {
-            m_RestClient.Proxy = null;
-        }
-
         internal delegate void LogDlg(string sender, string message);
-        internal LogDlg Log;
+        internal LogDlg? Log;
         internal bool LogResponseEnabled = false;
         internal bool LogExEnabled = false;
 
@@ -140,141 +84,48 @@ namespace RBTB_ServiceStrategy.Markets.Bybit
             }
         }
 
-        string SendRestRequest(IRequestContent message)
-        {
-            Method method;
-
-            switch (message.Method)
-            {
-                case RequestMethod.GET:
-                    method = Method.GET;
-                    break;
-                case RequestMethod.POST:
-                    method = Method.POST;
-                    break;
-                case RequestMethod.PUT:
-                    method = Method.PUT;
-                    break;
-                case RequestMethod.DELETE:
-                    method = Method.DELETE;
-                    break;
-                default:
-                    throw new NotImplementedException("Unknown request method");
-            }
-
-            var request = new RestRequest(message.Query, method);
-
-            if (message.Headers != null)
-            {
-                foreach (var header in message.Headers)
-                {
-                    request.AddHeader(header.Key, header.Value);
-                }
-            }
-
-            var r = m_RestClient.Execute(request);
-
-            return r.Content;
-        }
-
         #endregion
 
         #region [Public]
 
-        internal bool RequestPositionInfo(MarketCategory marketCategory, string symbol, out GetPositionInfoResponse response)
+        public OrderbookResult RequestGetOrderbook(MarketCategory category, string symbol)
         {
-            response = null;
-            var request_data = new GetPositionInfoRequest(marketCategory, symbol);
-
-            try
-            {
-                response = GetContent<GetPositionInfoResponse>(request_data);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                OnLogEx(ex, "");
-            }
-
-            return false;
-        }
-
-        internal bool RequestTickerInfo(string symbol, out GetOptionsTickersResponse response)
-        {
-            response = null;
-            var request = new GetOptionsTickersRequest() { BaseCoin = symbol };
-
-            try
-            {
-                response = GetContent<GetOptionsTickersResponse>(request);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                OnLogEx(ex, "");
-            }
-
-            return false;
-        }
-
-        internal bool RequestOrderbookInfo(MarketCategory category, string symbol, out GetOrderbookResponse response)
-        {
-            response = null;
             var request = new GetOrderbookRequest(category, symbol);
+            var response = GetContent<GetOrderbookResponse>(request);
 
-            try
-            {
-                response = GetContent<GetOrderbookResponse>(request);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                OnLogEx(ex, "");
-            }
-
-            return false;
+            return response.Result;
         }
 
-        internal bool RequestGetKlineInfo(MarketCategory category, string symbol, IntervalType intervalType, out GetKlineResponse response,
+        public KlineResult? RequestGetKline(MarketCategory category, string symbol, IntervalType intervalType,
             DateTime? start = null, DateTime? end = null, int limit = 100)
         {
-            response = null;
             var request = new GetKlineRequest(category, symbol, intervalType)
             {
                 StartTime = start,
                 EndTime = end,
                 Limit = Math.Min(limit, 1000)
             };
-            try
+
+            var response = GetContent<GetKlineResponse>(request);
+
+            if (response == null)
             {
-                response = GetContent<GetKlineResponse>(request);
-                return true;
+                OnLogEx(new NullReferenceException(message: nameof(GetKlineResponse)));
+                return null;
             }
-            catch (Exception ex)
+            if (response.RetCode != 0)
             {
-                OnLogEx(ex, "");
+                OnLogEx(new Exception(message: response.RetMsg));
             }
 
-
-            return false;
+            return response.Result;
         }
 
-        internal bool RequestGetServerTime(out ServerTimeResponse response)
+        public ServerTimeData RequestGetServerTime()
         {
-            response = null;
-            var request = new ServerTimeRequest();
-
-            try
-            {
-                response = GetContent<ServerTimeResponse>(request);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                OnLogEx(ex, "");
-            }
-
-            return false;
+            var requesrt = new ServerTimeRequest();
+            var response = GetContent<ServerTimeResponse>(requesrt);
+            return response.Result;
         }
 
         #endregion
